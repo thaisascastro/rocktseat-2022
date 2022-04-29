cb95671ac65a94e3fb27aee70c459f95b3cecba7
thaisascastro <thaisascastro@gmail.com>
2022-04-28 21:09:43 -0300

    feat: new class object

---
+++ js/object.js
@@ -0,0 +1,17 @@
+/* Object
+
+Propriedade / Atributos
+Funcionalidades / Métodos 
+
+{ propriedade : "valor" } 
+*/
+
+console.log({
+  // objeto
+  name: "Thaísa",
+  idade: 36,
+  // método
+  andar: function () {
+    console.log("andar");
+  },
+});

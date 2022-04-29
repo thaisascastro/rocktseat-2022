1cba33c6b25b909f3e4d41ca2f36a9c06f4431a3
thaisascastro <thaisascastro@gmail.com>
2022-04-28 21:17:08 -0300

    feat: new class undefined VS null

---
+++ js/undefinedVsNull.js
@@ -0,0 +1,11 @@
+/* Undefined VS Null */
+
+/*
+* undefined -> indefinido
+
+* null -> nulo, objeto que não possui nada dentro, diferente de indefinido
+*/
+
+console.log(null);
+console.log(undefined);
+console.log(null === undefined);

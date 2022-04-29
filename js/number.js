6211b7dde59a26e69696d7eabf8ea5df4eeeadb1
thaisascastro <thaisascastro@gmail.com>
2022-04-28 21:13:45 -0300

    feat: new class numbers

---
+++ js/number.js
@@ -0,0 +1,12 @@
+/* Numbers */
+
+/*
+33 // Inteiros
+2.4 // Reais
+Nan // Not a Number
+Infinity // Infinito
+*/
+
+console.log(33);
+console.log(33 / "asdff"); // Not a Number
+console.log(Infinity); // muito difícil aparecer, mas existe o Infinito.

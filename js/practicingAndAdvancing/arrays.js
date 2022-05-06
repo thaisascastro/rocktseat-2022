f647215d683eaab46132a52fec54618820817e75
thaisascastro <thaisascastro@gmail.com>
2022-05-05 21:17:53 -0300

    feat: arrays in practicing

---
+++ js/practicingAndAdvancing/arrays.js
@@ -0,0 +1,17 @@
+// Array
+
+const animals = [
+  "Lion",
+  "Monkey",
+  "Cats",
+  "Rats",
+  "Ratucho",
+  {
+    name: "Leandro",
+    age: 3,
+  },
+];
+
+console.log(animals[0]);
+console.log(animals.length);
+console.log(animals[5].name); // formas de evitar

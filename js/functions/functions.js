137d15f050cec7335f0a3b228e9eb4d485e46309
thaisascastro <thaisascastro@gmail.com>
2022-05-09 21:00:06 -0300

    feat: create function in functions

---
+++ js/functions/functions.js
@@ -0,0 +1,18 @@
+// criar um app de frases motivacionais
+
+// declaration - declaração de funções
+// function statement
+
+function createPhrases() {
+  console.log("Viver ou existir");
+  console.log("Ser ou não ser");
+  console.log("Faça o hoje!");
+}
+
+// executar a function
+// rodar, chamar, invocar
+// execute, call, invoke
+
+createPhrases();
+
+console.log("fim do programa!");

5ee74e95c71604c0423f9e5787e76fbf3900133a
thaisascastro <thaisascastro@gmail.com>
2022-05-12 20:48:59 -0300

    feat: practice

---
+++ js/practice/exercising2.js
@@ -0,0 +1,11 @@
+function multiplication(x, y) {
+  return x * y;
+}
+
+let n, m, res;
+
+console.log("Digite o primeiro número: ", n);
+console.log("Digite o segundo número: ", m);
+
+res < -multiplication;
+console.log("O resultado é: ", res);

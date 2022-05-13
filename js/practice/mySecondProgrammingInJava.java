2e159b468d92ca101d92bf61d58c77f2c0671d55
thaisascastro <thaisascastro@gmail.com>
2022-05-12 23:36:35 -0300

    feat: java init

---
+++ js/practice/mySecondProgrammingInJava.java
@@ -0,0 +1,28 @@
+class MeuSegundoProjeto {
+	
+	public static void main (String entrada[]) {
+		  //declaração de variáveis
+      int idade = 22;
+      char genero = 'F';
+      double peso = 64.1;
+      String nome = "Thaisa Da Silva Castro";
+      String curso = "Analise e Desenvolvimento de Sistemas";
+
+      System.out.println("eu sou o " + nome + "tenho " + idade + " anos e tenho " + peso + "kg de peso e curso " + curso) ;
+
+      System.exit(0) ;
+
+      	public static void main(String[] args) {
+		  //declaração de variáveis
+      int idade = 21;
+      char genero = 'M';
+      double peso = 75.8;
+      String nome = "Leandro Donizeti Bemfica Dias";
+      String curso = "Engenharia da Computação";
+
+      System.out.println("eu sou o " + nome + "tenho " + idade + " anos e tenho " + peso + "kg de peso e curso " + curso) ;
+
+		System.exit(0) ;
+	}
+	}
+}

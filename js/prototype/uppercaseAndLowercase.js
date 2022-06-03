d038a10e92e564fea30e75d0cc05c86474885b7f
thaisascastro <thaisascastro@gmail.com>
2022-06-02 21:19:49 -0300

    feat: to uppercase and lowercase

---
+++ js/prototype/uppercaseAndLowercase.js
@@ -0,0 +1,5 @@
+let word = "Programar é muito bacana!"
+console.log(word.toLocaleUpperCase()) // caixa alta
+
+let newWord = "Programar é muito legal!"
+console.log(word.toLocaleLowerCase()) // caixa baixa

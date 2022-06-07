117b52bf56d820307bf7d04476a071b8911df3bd
thaisascastro <thaisascastro@gmail.com>
2022-06-06 21:07:12 -0300

    feat: operations arithmetic

---
+++ js/expressionAndOperations/operatorsArithmetic.js
@@ -0,0 +1,29 @@
+// multiplication
+console.log(5 * 4)
+console.log(4.5 * 4.2)
+
+// division
+console.log(60 / 4)
+
+// subtraction
+console.log(87 - 7)
+
+// soma
+console.log(3001 + 2034)
+
+// division remainder - resto da divisão
+let remainder = 12 % 3
+console.log(remainder)
+
+// incrementing
+let incrementing = 0
+incrementing++
+console.log(incrementing)
+
+// decrement
+let decrement = 0
+decrement--
+console.log(decrement)
+
+// exponencial
+console.log(8 ** 5)

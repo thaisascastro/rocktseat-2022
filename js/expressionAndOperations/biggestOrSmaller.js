b0ecd4f4a26a7e9f98816bb683556d8e9b97fe38
thaisascastro <thaisascastro@gmail.com>
2022-06-09 19:51:19 -0300

    feat: big or small

---
+++ js/expressionAndOperations/biggestOrSmaller.js
@@ -0,0 +1,17 @@
+let one = 1
+let two = 2
+
+// maior que
+console.log( one > two) // false 
+
+// >= maior igual a
+console.log( one >= 1) // true
+console.log( two >= 1) // false
+
+// < menor que
+console.log(one < two) // false
+
+// <= menor igual a
+console.log( one <= two) // true
+console.log( one <= 1) // true
+console.log( one <= 0) // false

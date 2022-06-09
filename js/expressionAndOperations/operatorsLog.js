c777014922a7a87759d1e4ea622b28c165d37b4a
thaisascastro <thaisascastro@gmail.com>
2022-06-09 20:06:59 -0300

    operators log

---
+++ js/expressionAndOperations/operatorsLog.js
@@ -0,0 +1,14 @@
+// Operadores Lógicos
+
+let pao = true
+let queijo = true
+
+// AND &&
+console.log( pao && queijo)
+
+// OR ||
+console.log(pao || queijo)
+
+
+// NOT !
+console.log(!pao) // troca se é false or true

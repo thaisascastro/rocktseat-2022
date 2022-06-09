835ad3dc435d199f8ae8cfde3ad94abdc8927004
thaisascastro <thaisascastro@gmail.com>
2022-06-09 20:24:39 -0300

    falsy and Truthy

---
+++ js/expressionAndOperations/falsyAndTruthy.js
@@ -0,0 +1,29 @@
+// type conversion 
+
+/////// FALSY ///////
+
+/*
+ false
+0
+-0
+""
+null
+undefined
+NaN
+*/
+
+/////// TRUTHY ///////
+/*
+  true
+  {}
+  []
+  1
+  3.23
+  "0"
+  "false"
+  -1
+  Infinity
+  -Infinity
+*/
+
+console.log( true ? 'verdadeiro' : 'falso')

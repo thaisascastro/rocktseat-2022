713028ee9573fe272c5e2059a134e3832bc36781
thaisascastro <thaisascastro@gmail.com>
2022-06-09 20:36:46 -0300

    feat: initial new cap

---
+++ js/expressionAndOperations/precedenceOfOperators.js
@@ -0,0 +1,14 @@
+/* Operator precedence
+
+* grouping                     ()
+* negação e incremento         ! ++ ==
+* multiplicação e divisão      * /
+* adição e subtração           + - 
+* relacional                   < <= > >=C
+* igualdade                    == != === !==
+* AND                          &&
+* OR                           ||
+* condicional                  ?:
+* assignment (atribuição)      = += -= *=
+*/
+console.log( 3 > 2 && 2 > 1)

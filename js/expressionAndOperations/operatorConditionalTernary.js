5b76ebb33024d24fff4d71011849ef6e8ed31aa3
thaisascastro <thaisascastro@gmail.com>
2022-06-09 20:13:41 -0300

    operators ternary

---
+++ js/expressionAndOperations/operatorConditionalTernary.js
@@ -0,0 +1,24 @@
+// Operador Condicional (Ternário)
+
+let pao = true
+let queijo = true
+
+const niceBreakfast = pao && queijo ? 'Café top' : 'Café ruim'
+
+console.log(niceBreakfast)
+
+////////////////////////////////////
+
+let leite = true
+let paoDeQueijo = false
+
+const niceBreakfastNow = leite || paoDeQueijo ? 'Café top' : 'Café ruim'
+
+console.log(niceBreakfastNow)
+
+////////////////////////////////////
+
+let age = 16
+const canDrive = age >= 18 ? 'can drive' : "can't drive"
+console.log(canDrive)
+

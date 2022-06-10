fa0f813ec823dc6efe3dec881d01131710a0afbc
thaisascastro <thaisascastro@gmail.com>
2022-06-09 21:08:24 -0300

    if and else

---
+++ js/conditionals/ifAndElse.js
@@ -0,0 +1,11 @@
+// if...else
+
+let temperature = 37;
+
+if (temperature >= 37.5) {
+    console.log('Febre alta')
+} else if (temperature < 37.5 && temperature >= 37) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}

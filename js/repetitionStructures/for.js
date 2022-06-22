92f8f9e5407dddbe730c7c83dea7f0dbacb9da91
thaisascastro <thaisascastro@gmail.com>
2022-06-21 21:02:01 -0300

    feat: structures for

---
+++ js/repetitionStructures/for.js
@@ -0,0 +1,11 @@
+// Estrutura de Repetição
+
+// break = para a continuação do loop
+// continue = pula a execução no momento
+
+for (i = 10; i > 0; i--) {
+  if ( i === 5) {
+    continue;
+  }
+  console.log(i)
+}

62b10c8866aa5f8f0abc99e90869f290302614b4
thaisascastro <thaisascastro@gmail.com>
2022-06-27 18:48:05 -0300

    feat: sidebar

--- ignite/fundamentalsReact/src/App.jsx
+++ ignite/fundamentalsReact/src/App.jsx
@@ -1,13 +1,22 @@
 import { Fragment } from "react"
 import { Header } from "./components/Header";
-import Post from "./Post"
+import Post from "./Post";
 import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
   return (
     <Fragment>
       <Header/>
+      
+     <div className={styles.wrapper}>
+      <Sidebar/>
+      <aside>
+        sidebar
+      </aside>
+      <main>
       <Post
         author="Thaísa Castro"
         content="Trazendo as propriedade dentro de um objeto"
@@ -16,6 +25,8 @@ export function App() {
         author="Leandro Dias"
         content="Trazendo novo seguidor"
       />
+      </main>
+     </div>
    </Fragment>
   )
 }

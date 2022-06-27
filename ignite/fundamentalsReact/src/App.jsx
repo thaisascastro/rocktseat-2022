fbc6baac5e2e9cd06d0a7760047bb26e3d87299f
thaisascastro <thaisascastro@gmail.com>
2022-06-27 17:47:39 -0300

    feat: structure react

--- ignite/fundamentalsReact/src/App.jsx
+++ ignite/fundamentalsReact/src/App.jsx
@@ -1,10 +1,13 @@
 import { Fragment } from "react"
+import { Header } from "./components/Header";
 import Post from "./Post"
+import './global.css';
 
 export function App() {
 
   return (
     <Fragment>
+      <Header/>
       <Post
         author="Thaísa Castro"
         content="Trazendo as propriedade dentro de um objeto"

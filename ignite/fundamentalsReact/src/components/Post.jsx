c78a70724a10428bb4dbebf268a8f3f6e37a4ff4
thaisascastro <thaisascastro@gmail.com>
2022-07-04 21:12:01 -0300

    style: style post

--- ignite/fundamentalsReact/src/components/Post.jsx
+++ ignite/fundamentalsReact/src/components/Post.jsx
@@ -15,14 +15,18 @@ export function Post() {
     <time title="30 de Junho às 21:03h" dateTime="2022-06-30 00:13:30">Publicado há 1h</time>
     </header>
 
-    <div style={styles.content}>
+    <div className={styles.content}>
       <p>Fala galeraa 👋</p>
 
       <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
 
-      <p>👉 <a href="">jane.design/doctorcare</a></p>
+      <p><a href="">jane.design/doctorcare</a></p>
 
-      <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+      <p>
+        <a href="">#novoprojeto </a>{" "}
+        <a href="">#nlw</a>{" "}
+        <a href="">#rocketseat</a>
+        </p>
     </div>
    </article>
   )

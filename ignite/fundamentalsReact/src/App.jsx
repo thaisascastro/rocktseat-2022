fa300ed56ab42e063f2c4ee34c7366b6dc552698
thaisascastro <thaisascastro@gmail.com>
2022-09-01 21:05:54 -0300

    feat: the motors in jsx

--- ignite/fundamentalsReact/src/App.jsx
+++ ignite/fundamentalsReact/src/App.jsx
@@ -7,6 +7,49 @@ import { Sidebar } from "./components/Sidebar";
 import './global.css';
 import styles from './App.module.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/58372020?v=4',
+      name: 'Thaísa Castro',
+      role: 'Web Developer'
+    },
+    content: [
+      {
+        type: 'paragraph', content: 'Fala galeraa 👋<'
+      },
+      {
+        type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link', content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-09-01 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/47450207?v=4',
+      name: 'Leandro Dias',
+      role: 'Web Developer'
+    },
+    content: [
+      {
+        type: 'paragraph', content: 'Fala galeraa 👋<'
+      },
+      {
+        type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      {
+        type: 'link', content: 'jane.design/doctorcare'
+      }
+    ],
+    publishedAt: new Date('2022-09-01 21:02:00')
+  }
+]
+
 export function App() {
 
   return (
@@ -16,14 +59,9 @@ export function App() {
      <div className={styles.wrapper}>
       <Sidebar/>
       <main>
-      <Post
-        author="Thaísa Castro"
-        content="Trazendo as propriedade dentro de um objeto"
-        />
-      <Post
-        author="Leandro Dias"
-        content="Trazendo novo seguidor"
-      />
+     {posts.map(post => {
+      <Post/>
+     })}
       </main>
      </div>
    </Fragment>

c5256689b8b213828986d65035a1e9eedf9fe992
thaisascastro <thaisascastro@gmail.com>
2022-09-05 21:31:31 -0300

    feat: React Engines - State (useState)

--- ignite/fundamentalsReact/src/components/Post.jsx
+++ ignite/fundamentalsReact/src/components/Post.jsx
@@ -1,10 +1,18 @@
 import { format, formatDistanceToNow } from "date-fns";
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from "react";
 import styles from "./Post.module.css";
 import { Comment } from "./Comment.jsx";
 import { Avatar } from "./Avatar";
 
+
 export function Post({ author, publishedAt, content}) {
+
+  const [comments, setComments] = useState([
+    1,
+    2
+  ])
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   })
@@ -13,6 +21,13 @@ export function Post({ author, publishedAt, content}) {
     locale: ptBR,
     addSuffix: true,
   })
+
+  function handleCreateNewComment() {
+    event?.preventDefault()
+
+    setComments([...comments, comments.length + 1]);
+
+  }
 
   return (
    <article className={styles.post}>
@@ -38,21 +53,22 @@ export function Post({ author, publishedAt, content}) {
     })}
     </div>
 
-    <form className={styles.commentForm}>
+    <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
       <strong>Deixe seu feedback</strong>
 
         <textarea
         placeholder="Deixe um comentário"
         />
+
         <footer>
           <button type="submit">Publicar</button>
         </footer>
     </form>
 
     <div className={styles.commentList}>
-      <Comment/>
-      <Comment/>
-      <Comment/>
+     {comments.map(comment => {
+      return <Comment/>
+     })}
     </div>
    </article>
   )

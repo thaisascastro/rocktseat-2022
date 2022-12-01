e4160dc16aa1774c2e46919546226f6c5683b66b
thaisascastro <thaisascastro@gmail.com>
2022-12-01 16:08:39 -0300

    feat: closures

--- ignite/fundamentalsReact/src/components/Comment.jsx
+++ ignite/fundamentalsReact/src/components/Comment.jsx
@@ -1,13 +1,20 @@
 import styles from './Comment.module.css';
 import { ThumbsUp, Trash} from 'phosphor-react';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 
 export function Comment({content, onDeleteComment}) {
 
-  function handleDeleteComment() {
+  const [likeCount, setlikeCount] = useState(0)
 
-    onDeleteComment(content)
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setlikeCount((state) => {
+      return state + 1});
   }
 
   return (
@@ -31,9 +38,9 @@ export function Comment({content, onDeleteComment}) {
       </div>
       
       <footer>
-        <button>
+        <button onClick={handleLikeComment}>
           <ThumbsUp/>
-          Aplaudir <span>20</span>
+          Aplaudir <span>{likeCount}</span>
         </button>
       </footer>
       </div>

5c42d090a827f11d9c2c709dc6db2721d911814c
thaisascastro <thaisascastro@gmail.com>
2022-12-03 14:20:06 -0300

    feat: configuration themes

--- ignite/project-spas-react/src/App.tsx
+++ ignite/project-spas-react/src/App.tsx
@@ -1,14 +1,15 @@
-import { Fragment, useState } from 'react'
+import { ThemeProvider } from "styled-components";
 import { Button } from './components/button'
+import { defaultTheme } from "./styles/themes/default";
 
 export default function App() {
   return (
-   <Fragment>
+   <ThemeProvider theme={defaultTheme}>
     <Button variant='primary' />
     <Button variant='secondary'/>
     <Button variant='success'/>
     <Button variant='danger'/>
-   </Fragment>
+   </ThemeProvider>
   )
 }
 
---
+++ ignite/project-spas-react/src/styles/themes/default.ts
@@ -0,0 +1,5 @@
+export const defaultTheme = {
+  primary: '#6959CD',
+  secondary: 'orange',
+  white: '#FFF'
+}

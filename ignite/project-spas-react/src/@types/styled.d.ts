a217ade91ea5dcf8dddcf5454d97ccfd1ad7a79a
thaisascastro <thaisascastro@gmail.com>
2022-12-06 21:29:50 -0300

    feat: default themes

---
+++ ignite/project-spas-react/src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
+
+type ThemeType = typeof defaultTheme; 
+
+// sobreescrever algo do styled-components
+declare module 'styled-components' {
+  export interface DefaultTheme extends ThemeType {}
+}

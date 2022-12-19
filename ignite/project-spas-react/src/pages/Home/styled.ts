c95d2d59fcf337bac5ee4c9b038953470fc19228
thaisascastro <thaisascastro@gmail.com>
2022-12-19 12:39:33 -0300

    feat: solution about freight-packing-list active/passive

--- ignite/project-spas-react/src/pages/Home/styled.ts
+++ ignite/project-spas-react/src/pages/Home/styled.ts
@@ -90,6 +90,15 @@ const BaseInput = styled.input`
   font-size: 1.125rem;
   padding: 0 0.5rem;
   color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color:  color: ${(props) => props.theme['gren-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 export const TaskInput = styled(BaseInput)`
   flex: 1;
